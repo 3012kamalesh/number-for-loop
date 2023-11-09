d2672df38a7c387b02f96031d75d42eb3e92fc38
3012kamalesh <143986667+3012kamalesh@users.noreply.github.com>
2023-11-09 10:27:43 +0530

    number

---
+++ number.py
@@ -0,0 +1,20 @@
+##num=int(input("enter the number"))
+##for i in range(1,num+1):
+##    for j in range(1,i+1):
+##        print(i,end=" ")
+##    print()
+
+
+##num=int(input("enter the number"))
+##for i in range(num,0,-1):
+##    for j in range(1,i+1):
+##        print(j,end=" ")
+##    print()
+
+num=int(input("enter the number"))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
